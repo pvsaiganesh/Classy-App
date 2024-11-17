5637080ba98e307edad98ffcc402fa1355beea61
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-11-17 23:13:45 +0530

    up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,74 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  StyleSheet,
+  Platform,
+  Image,
+  ImageBackground,
+  Dimensions,
+  View,
+} from "react-native";
+import { Link, SplashScreen, useRootNavigationState } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import * as Font from "expo-font";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
-
+const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  backgroundImage: {
+    flex: 1,
+    width: "100%", // Full width
   },
 });
+SplashScreen.preventAutoHideAsync();
+
+export default function Page() {
+  const rootNavigationState = useRootNavigationState();
+  const [appIsReady, setAppIsReady] = useState(false);
+  const onPress = () => {};
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!rootNavigationState?.key) {
+    return null;
+  }
+  return (
+    <View style={styles.container}>
+      <Link href="/onboarding">
+        <ImageBackground
+          source={require("./../../assets/images/splash-screen.png")} // Replace with your image path
+          style={[styles.backgroundImage, { width: width, height: height }]}
+        >
+          {/* Your other components go here */}
+        </ImageBackground>
+      </Link>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,77 +1,77 @@
-// import { useCallback, useEffect, useState } from "react";
-// import { Text, TouchableOpacity, View } from "react-native";
-// import Entypo from "@expo/vector-icons/Entypo";
-// import * as SplashScreen from "expo-splash-screen";
-// import * as Font from "expo-font";
-// import {
-//   StyleSheet,
-//   Platform,
-//   Image,
-//   ImageBackground,
-//   Dimensions,
-// } from "react-native";
-// import { Link, Stack, useRootNavigationState } from "expo-router";
-// import { Collapsible } from "@/components/Collapsible";
-// import { ExternalLink } from "@/components/ExternalLink";
-// import ParallaxScrollView from "@/components/ParallaxScrollView";
-// import { ThemedText } from "@/components/ThemedText";
-// import { ThemedView } from "@/components/ThemedView";
-// import { IconSymbol } from "@/components/ui/IconSymbol";
-// import { SafeAreaView } from "react-native-safe-area-context";
-// // Keep the splash screen visible while we fetch resources
-// SplashScreen.preventAutoHideAsync();
+import { useCallback, useEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import {
+  StyleSheet,
+  Platform,
+  Image,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+import { Link, Stack, useRootNavigationState } from "expo-router";
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { SafeAreaView } from "react-native-safe-area-context";
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
-// // Set the animation options. This is optional.
-// // SplashScreen.setOptions({
-// //   duration: 1000,
-// //   fade: true,
-// // });
-// const { width, height } = Dimensions.get("window");
-// export default function App() {
-//   const rootNavigationState = useRootNavigationState();
-//   const [appIsReady, setAppIsReady] = useState(false);
-//   const onPress = () => {};
-//   useEffect(() => {
-//     async function prepare() {
-//       try {
-//         // Pre-load fonts, make any API calls you need to do here
-//         await Font.loadAsync(Entypo.font);
-//         // Artificially delay for two seconds to simulate a slow loading
-//         // experience. Please remove this if you copy and paste the code!
-//         await new Promise((resolve) => setTimeout(resolve, 2000));
-//       } catch (e) {
-//         console.warn(e);
-//       } finally {
-//         // Tell the application to render
-//         setAppIsReady(true);
-//       }
-//     }
+// Set the animation options. This is optional.
+// SplashScreen.setOptions({
+//   duration: 1000,
+//   fade: true,
+// });
+const { width, height } = Dimensions.get("window");
+export default function App() {
+  const rootNavigationState = useRootNavigationState();
+  const [appIsReady, setAppIsReady] = useState(false);
+  const onPress = () => {};
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
 
-//     prepare();
-//   }, []);
+    prepare();
+  }, []);
 
-//   const onLayoutRootView = useCallback(() => {
-//     if (appIsReady) {
-//       // This tells the splash screen to hide immediately! If we call this after
-//       // `setAppIsReady`, then we may see a blank screen while the app is
-//       // loading its initial state and rendering its first pixels. So instead,
-//       // we hide the splash screen once we know the root view has already
-//       // performed layout.
-//       SplashScreen.hide();
-//     }
-//   }, [appIsReady]);
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
 
-//   if (!rootNavigationState?.key) {
-//     return null;
-//   }
-//   return (
-//     <Stack screenOptions={{ headerShown: false }}>
-//       {/* <Stack.Screen name="(tabs)" options={{}} /> */}
-//       <Stack.Screen name="splash" options={{}} />
-//       <Stack.Screen name="details" options={{}} />
-//     </Stack>
-//   );
-// }
+  if (!rootNavigationState?.key) {
+    return null;
+  }
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* <Stack.Screen name="(tabs)" options={{}} /> */}
+      <Stack.Screen name="(tabs)" options={{}} />
+      <Stack.Screen name="details" options={{}} />
+    </Stack>
+  );
+}
 
 // import * as SplashScreen from "expo-splash-screen";
 // import React, { useCallback, useEffect, useState } from "react";
