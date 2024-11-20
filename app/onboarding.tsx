d06dd2562b578a0fb70954289bc671f88f3d6414
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-11-20 13:05:44 +0530

    Update onboarding.tsx

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -6,58 +6,38 @@ import {
   ImageBackground,
   Dimensions,
   View,
+  Text,
 } from "react-native";
 import { Link } from "expo-router";
 import Logo from "../assets/logo";
 const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
-  container: { flex: 1,  marginHorizontal: "auto" },
+  container: { flex: 1,  },
   backgroundImage: {
-    flex: 2, 
+    flex:1
   },
-  row:{
-    flexDirection:"row",
+  image:{
+    flex:1,
   }
 });
-
-const Row = ({ children }) => (
-  <View style={styles.row}>{children}</View>
-)
-const Col = ({ numRows, children }) => {
-  return  (
-    <View style={styles[`${numRows}col`]}>{children}</View>
-  )
-}
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      
         <ImageBackground
           source={require("./../assets/images/onboarding-bg-1.png")} // Replace with your image path
-          style={[styles.backgroundImage, { width: width, height: height }]}
+          style={[styles.backgroundImage, { width: width, height: height/2 }]}
         >
-      <Row>
-        <Col numRows={1}>
-        <Image style={[styles.backgroundImage, { width: width/4, height: height/4 }]} source={require("./../assets/images/onboarding-1.png")}/>
-            </Col>
-            </Row>
-            <Row>
-            <Col numRows={1}><Text>hell</Text></Col>
-        
-        <Col numRows={1}>
-            <Image style={[styles.backgroundImage, { width: width/4, height: height/4 }]} source={require("./../assets/images/onboarding-2.png")}/>
-            </Col>
-<Col numRows={1}></Col>
-      </Row>
-            
+            <Image style={styles.image} source={require("./../assets/images/onboarding-1.png")}/>
+            <Image style={styles.image} source={require("./../assets/images/onboarding-2.png")}/>
+<View style={styles.image}><Text style={styles.baseText}>hello</Text></View>
         </ImageBackground>
         <ImageBackground
           source={require("./../assets/images/onboarding-bg-2.png")} // Replace with your image path
-          style={[styles.backgroundImage, { width: width, height: height }]}
+          style={[styles.backgroundImage, { width: width, height: height/2 }]}
         >
-            <Image style={[styles.backgroundImage, { width: width/4, height: height/4 }]} source={require("./../assets/images/onboarding-3.png")}/>
-            <Image style={[styles.backgroundImage, { width: width/4, height: height/4 }]} source={require("./../assets/images/onboarding-4.png")}/>
+            <Image style={styles.image} source={require("./../assets/images/onboarding-3.png")}/>
+            <Image style={styles.image} source={require("./../assets/images/onboarding-4.png")}/>
 
         </ImageBackground>
     </View>
