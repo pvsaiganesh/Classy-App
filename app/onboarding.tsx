e397951fe51cec2bfef1ef77ebbb7a301b8b0e6a
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-11-25 18:52:14 +0530

    up

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -13,45 +13,55 @@ import { Link, useNavigation } from "expo-router";
 // import { StatusBar } from "expo-status-bar";
 import Logo from "../assets/logo";
 import { Button, IconButton, MD3Colors, useTheme } from "react-native-paper";
-import { OnboardingImage } from "../assets/images/onboarding-image";
+import { OnboardingImage } from "../assets/images/onboarding-image.jsx";
 
 const { width, height } = Dimensions.get("window");
-const styles = StyleSheet.create({
-  app: { flex: 1, backgroundColor: "#fff", padding: 50 },
-  appContainer: { flex: 1, padding: 10, paddingTop: 50 },
-  appContainer2: {
-    flex: 1,
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-  },
-  row: { flexDirection: "row" },
-  baseText: {
-    fontFamily: "mont-serrat",
-    fontSize: 24,
-  },
-  container: {
-    flex: 1,
-    padding: 10,
-    width: "50%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  backgroundImage: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-  },
-});
 
 export default function Page() {
   const theme = useTheme();
   const navigation = useNavigation();
+  const styles = StyleSheet.create({
+    app: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    skip: { flex: 1, textAlign: "right" },
+    image: { flex: 1, textAlign: "center" },
+    heading: { flex: 1, textAlign: "center" },
+    textBlack: { fontSize: 24, textAlign: "center", alignItems: "center" },
+    textOrange: { color: theme.colors.primary, fontSize: 24 },
+    bgOrange: { backgroundColor: theme.colors.primary },
+    desc: { flex: 1, fontSize: 16, textAlign: "center" },
+    button: { flex: 1, textAlign: "center" },
+  });
   return (
     <View style={styles.app}>
-      <OnboardingImage />
+      <View style={styles.skip}>
+        <Text>Skip</Text>
+      </View>
+      <View style={styles.image}>
+        <OnboardingImage />
+      </View>
+      <View style={styles.heading}>
+        <Text style={styles.textBlack}>
+          Shop <Text style={styles.textOrange}>Smart</Text>
+          {"   "}
+          Shop <Text style={styles.textOrange}>Exciting</Text>
+        </Text>
+
+        <Text style={styles.textBlack}>
+          Shop
+          <Image
+            style={styles.bgOrange}
+            source={require("../assets/images/orange-text.png")}
+          />
+        </Text>
+        <Text style={styles.desc}>
+          Find your daily necessities at Brand. The world's largest fashion
+          e-commerce has arrived in a mobile version. Shop now!
+        </Text>
+      </View>
     </View>
   );
 }
