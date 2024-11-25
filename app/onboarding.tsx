88e85634b6a7ab420a6c06b7ebbeb09c04116f7d
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-11-25 19:59:06 +0530

    Update onboarding.tsx

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -23,20 +23,26 @@ export default function Page() {
     app: {
       flex: 1,
       justifyContent: "flex-start",
-      alignItems: "center",
+      alignItems: "flex-end",
       textAlign: "center",
+      paddingTop: 50,
     },
     skip: {
       flexDirection: "row",
+      paddingEnd: 30,
+      fontSize: 30,
       justifyContent: "flex-end",
       textAlign: "right",
     },
     image: {
+      paddingTop: 50,
+      paddingStart: 30,
       flex: 1,
       textAlign: "center",
       flexDirection: "row",
       justifyContent: "center",
       alignItems: "center",
+      width: width,
     },
     container: {
       flex: 1,
@@ -56,41 +62,45 @@ export default function Page() {
       color: theme.colors.secondary,
     },
     desc: { fontSize: 16, textAlign: "center" },
-    button: { textAlign: "center" },
+    button: { textAlign: "center", alignSelf: "stretch" },
   });
   return (
     <View style={styles.app}>
       <StatusBar />
-      <View style={styles.skip}>
-        <Text>Skip</Text>
-      </View>
-      <View style={[styles.image, { width: width, height: height / 2 }]}>
-        <OnboardingImage />
-      </View>
-      <View style={styles.container}>
-        <Text style={styles.textBlack}>
-          Shop <Text style={styles.textOrange}>Smart</Text>
-          {"   "}
-          Shop <Text style={styles.textOrange}>Exciting</Text>
-        </Text>
+      <View>
+        <View style={styles.skip}>
+          <Text>Skip</Text>
+        </View>
+        <View style={[styles.image, { width: width, height: height / 2 }]}>
+          <OnboardingImage />
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.textBlack}>
+            Shop <Text style={styles.textOrange}>Smart</Text>
+            {"   "}
+            Shop <Text style={styles.textOrange}>Exciting</Text>
+          </Text>
 
-        <Text style={styles.textBlack}>Shop{"  "}</Text>
-        <Image
-          style={{ ...styles.bgOrange, paddingTop: 30 }}
-          source={require("../assets/images/orange-text.png")}
-        />
-        <Text style={styles.desc}>
-          Find your daily necessities at Brand. The world's largest fashion
-          e-commerce has arrived in a mobile version. Shop now!
-        </Text>
-        <Button
-          mode="contained"
-          labelStyle={{ color: "white", fontSize: 16 }}
-          style={styles.bgOrange}
-        >
-          Login
-        </Button>
-        <Button labelStyle={{ color: "black", fontSize: 16 }}>Register</Button>
+          <Text style={styles.textBlack}>Shop{"  "}</Text>
+          <Image
+            style={{ ...styles.bgOrange, paddingTop: 0 }}
+            source={require("../assets/images/orange-text.png")}
+          />
+          <Text style={styles.desc}>
+            Find your daily necessities at Brand. The world's largest fashion
+            e-commerce has arrived in a mobile version. Shop now!
+          </Text>
+          <Button
+            mode="contained"
+            labelStyle={{ color: "white", fontSize: 16 }}
+            style={{ ...styles.bgOrange, ...styles.button }}
+          >
+            Login
+          </Button>
+          <Button labelStyle={{ color: "black", fontSize: 16 }}>
+            Register
+          </Button>
+        </View>
       </View>
     </View>
   );
