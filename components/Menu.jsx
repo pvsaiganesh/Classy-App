2c8c4fd1072b01a804cb65157ed2272ce157febd
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-12-12 13:59:04 +0530

    Update Menu.jsx

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, Text, StyleSheet, Dimensions, Pressable } from "react-native";
+import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  StatusBar,
+} from "react-native";
 import theme from "../app/theme";
 import { Avatar, Button, Drawer, IconButton } from "react-native-paper";
 import Winner from "../assets/images/Winner";
@@ -8,6 +15,7 @@ import Cart from "../assets/images/Cart";
 import Coupon from "../assets/images/Coupon";
 import { ScrollView } from "react-native-gesture-handler";
 import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 const { width, height } = Dimensions.get("window");
 
 const Col = ({ numRows = 4, children }) => {
@@ -71,6 +79,7 @@ const styles = StyleSheet.create({
   app: {
     // flex: 1,
     flex: 8,
+
     padding: 20,
     gap: 10,
     marginTop: 30,
@@ -103,6 +112,7 @@ const styles = StyleSheet.create({
 });
 const Menu = ({ navigation }) => {
   const [active, setActive] = React.useState("");
+
   return (
     <ScrollView styles={styles.app}>
       <Row>
@@ -117,9 +127,9 @@ const Menu = ({ navigation }) => {
                 backgroundColor: theme.colors.primaryContainer,
                 paddingTop: 60,
 
-                paddingStart: 20,
-                paddingEnd: 20,
-                paddingBottom: 20,
+                paddingStart: 30,
+                paddingEnd: 30,
+                paddingBottom: 30,
 
                 textAlign: "start",
               }}
@@ -144,87 +154,88 @@ const Menu = ({ navigation }) => {
           </Pressable>
         </Col>
       </Row>
-      <Row>
-        <Col numRows={8}>
-          <Drawer.Section
-            style={{ marginTop: 20, margin: 10, gap: 20 }}
-            showDivider={false}
-          >
-            <Drawer.Item
-              label="Lucky Draw"
-              icon={(props) => <Winner />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-            <Drawer.Item
-              label="Notifications"
-              icon={(props) => <Bell />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-            <Drawer.Item
-              label="Wishlist"
-              icon={({ color }) => <IconButton icon="heart" color={color} />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-            <Drawer.Item
-              label="Orders"
-              icon={(props) => <Winner />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-            <Drawer.Item
-              label="Cart"
-              icon={(props) => <Cart />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-            <Drawer.Item
-              label="Coupons"
-              icon={(props) => <Coupon />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-            <Drawer.Item
-              label="How It Works"
-              icon={(props) => <Winner />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-            <Drawer.Item
-              label="Rules and guidelines"
-              icon={(props) => <Winner />}
-              active={active === "first"}
-              onPress={() => setActive("first")}
-            />
-          </Drawer.Section>
-        </Col>
-      </Row>
-      <Row style={{ marginTop: 20, margin: 10, gap: 20, alignSelf: "center" }}>
-        <Col numRows={4}>
-          <Button
-            onPress={() => {
-              router.push("/login");
-            }}
-            mode="contained"
-            labelStyle={{ color: "white", fontSize: 16 }}
-            style={{ ...styles.bgOrange, ...styles.button }}
-          >
-            Login
-          </Button>
-        </Col>
-        <Col numRows={4}>
-          <Button
-            onPress={() => {
-              router.push("/register");
-            }}
-            labelStyle={{ color: "black", fontSize: 16 }}
-          >
-            Register
-          </Button>
-        </Col>
-      </Row>
+      <SafeAreaView>
+        <Row>
+          <Col numRows={8}>
+            <Drawer.Section style={{ gap: 20 }} showDivider={false}>
+              <Drawer.Item
+                label="Lucky Draw"
+                icon={(props) => <Winner />}
+                active={active === "(luckydraw)"}
+                onPress={() => setActive("(luckydraw)")}
+              />
+              <Drawer.Item
+                label="Notifications"
+                icon={(props) => <Bell />}
+                active={active === "(notifications)"}
+                onPress={() => setActive("(notifications)")}
+              />
+              <Drawer.Item
+                label="Wishlist"
+                icon={({ color }) => <IconButton icon="heart" color={color} />}
+                active={active === "(myorders)"}
+                onPress={() => setActive("(myorders)")}
+              />
+              <Drawer.Item
+                label="My Orders"
+                icon={(props) => <Winner />}
+                active={active === "(myorders)"}
+                onPress={() => setActive("first")}
+              />
+              <Drawer.Item
+                label="My Cart"
+                icon={(props) => <Cart />}
+                active={active === "(mycart)"}
+                onPress={() => setActive("(mycart)")}
+              />
+              <Drawer.Item
+                label="My Coupons"
+                icon={(props) => <Coupon />}
+                active={active === "(mycoupons)"}
+                onPress={() => setActive("(mycoupons)")}
+              />
+              <Drawer.Item
+                label="How It Works"
+                icon={(props) => <Winner />}
+                active={active === "(howitworks)"}
+                onPress={() => setActive("(howitworks)")}
+              />
+              <Drawer.Item
+                label="Rules and guidelines"
+                icon={(props) => <Winner />}
+                active={active === "(rulesandregulations)"}
+                onPress={() => setActive("(rulesandregulations)")}
+              />
+            </Drawer.Section>
+          </Col>
+        </Row>
+        <Row
+          style={{ marginTop: 20, margin: 10, gap: 20, alignSelf: "center" }}
+        >
+          <Col numRows={4}>
+            <Button
+              onPress={() => {
+                router.push("/login");
+              }}
+              mode="contained"
+              labelStyle={{ color: "white", fontSize: 16 }}
+              style={{ ...styles.bgOrange, ...styles.button }}
+            >
+              Login
+            </Button>
+          </Col>
+          <Col numRows={4}>
+            <Button
+              onPress={() => {
+                router.push("/register");
+              }}
+              labelStyle={{ color: "black", fontSize: 16 }}
+            >
+              Register
+            </Button>
+          </Col>
+        </Row>
+      </SafeAreaView>
     </ScrollView>
   );
 };
