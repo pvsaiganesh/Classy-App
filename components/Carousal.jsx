07b417bbbb6a69f1423765321ab6d1ad7864c2f9
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-12-14 17:11:21 +0530

    Revert "up"
    This reverts commit 02b0af964233d2f10f85000a60cec6b891d609d3.

--- components/Carousal.jsx
+++ components/Carousal.jsx
@@ -1,55 +1,55 @@
 import * as React from "react";
 import { Dimensions, Text, View, Image, TouchableOpacity } from "react-native";
-import Carousel, { ParallaxImage } from "react-native-snap-carousel";
-import { Dimensions, StyleSheet } from "react-native";
+import Carousel, { Pagination } from "react-native-reanimated-carousel";
 import { imageCarousalArray } from "../utils/constants";
 
 function CarousalComp() {
   const width = Dimensions.get("window").width;
-
-  _renderItem = ({ item, index }) => {
-    return (
-      <View style={styles.item}>
-        <ParallaxImage
-          source={{ uri: item.thumbnail }}
-          containerStyle={styles.imageContainer}
-          style={styles.image}
-          parallaxFactor={0.4}
-          {...parallaxProps}
-        />
-        <Text style={styles.title} numberOfLines={2}>
-          {item.title}
-        </Text>
-      </View>
-    );
-  };
-
   return (
-    <Carousel
-      sliderWidth={screenWidth}
-      sliderHeight={screenWidth}
-      itemWidth={screenWidth - 60}
-      data={imageCarousalArray}
-      renderItem={this._renderItem}
-      hasParallaxImages={true}
-    />
+    <View style={{ flex: 1 }}>
+      <Carousel
+        loop
+        width={width}
+        autoPlayInterval={2000}
+        // withAnimation
+        pagingEnabled={true}
+        enabled={true}
+        mode="parallax"
+        height={width / 2}
+        autoPlay={true}
+        data={imageCarousalArray}
+        scrollAnimationDuration={3000}
+        onSnapToItem={(index) => console.log("current index:", index)}
+        modeConfig={{
+          parallaxScrollingScale: 0.9,
+          parallaxScrollingOffset: 50,
+        }}
+        renderItem={({ item }) => {
+          // console.log(item);
+          return (
+            <View
+              style={{
+                flex: 1,
+                borderWidth: 1,
+                justifyContent: "center",
+              }}
+            >
+              <TouchableOpacity>
+                <Image
+                  style={{
+                    width: "100%", // Set to match carousel width
+                    height: "100%", // Set to match carousel height
+                    resizeMode: "cover", // Ensure the image fits nicely
+                  }}
+                  source={item?.imageUrl}
+                />
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 }
-const styles = StyleSheet.create({
-  item: {
-    width: screenWidth - 60,
-    height: screenWidth - 60,
-  },
-  imageContainer: {
-    flex: 1,
-    marginBottom: Platform.select({ ios: 0, android: 1 }), // Prevent a random Android rendering issue
-    backgroundColor: "white",
-    borderRadius: 8,
-  },
-  image: {
-    ...StyleSheet.absoluteFillObject,
-    resizeMode: "cover",
-  },
-});
 
 export default CarousalComp;
